fde8b75c3fab6657d122763b7a6cd38ef95be7ca
Edneth Girão <erineu16@gmail.com>
2021-10-29 09:44:02 +0100

    Alteracao InterfaceUsuario

--- interfaceusuario/index.php
+++ interfaceusuario/index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="pt-PT">
-<<<<<<< HEAD
 
 <head>
     <meta charset="UTF-8">
@@ -17,6 +16,18 @@
             <span id="btnMenu">&#9776;</span>
         </div>
         <div class="lista">
+            <div class="elLista">
+                <img class="bx-search" src="../icons/search-circle-outline.svg" alt="pesquisar">
+                <input type="text" placeholder="pesquisar...">
+                <span class="nome">Pesquisar</span>
+            </div>
+            <div class="elLista">
+                <a href="#">
+                    <img src="../icons/grid-outline.svg">
+                    <span class="nomeLink">Painel</span>
+                </a>
+                <span class="nome">Painel</span>
+            </div>
             <div class="elLista">
                 <a href="javascript:void(0);">
                     <img src="../icons/person-sharp - cópia.svg" alt="Perfil">
@@ -38,82 +49,20 @@
                 </a>
                 <span class="nome">Histórico</span>
             </div>
-            <div class="elLista">
-                <a href="javascript:void(0);">
-                    <img src="../icons/settings-outline - cópia.svg" alt="Configurações">
-                    <span class="nomeLink">Configurações</span>
-                </a>
-                <span class="nome">Configurações</span>
-            </div>
             <div class="elLista perfil">
                 <div class="detalhesPerfil">
                     <img src="../icons/person-sharp - cópia.svg" alt="Imagem do Perfil" id="fotoPerfil">
                     <div>
-                        <div class="nome"></div>
-                        <div class="profissao"></div>
+                        <div class="nomePerfil">Mauro Neto</div>
+                        <div class="profissao">Dj</div>
                     </div>
-=======
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title> Interface do Usuário </title>
-        <link rel="stylesheet" type="text/css" href="../css/estilo.css">
-    </head>
-    <body>
-        <div class="snav french-blue">
-            <div class="logo">
-                <img src="../imagens/Logomarca-Pramaactualizada.png" alt="Logotipo" class="icon1">
-                <span id="btnMenu">&#9776;</span>
-            </div>
-            <div class="lista">
-                <div class="elLista">
-                    <img class="bx-search" src="../icons/search-circle-outline - cópia.svg" alt="pesquisar">
-                    <input type="text" placeholder="pesquisar...">
-                    <span class="nome">Pesquisar</span>
-                </div>
-                <div class="elLista">
-                    <a href="#">
-                        <img src="../icons/grid-outline.svg">
-                        <span class="nomeLink">Painel</span>
-                    </a>
-                    <span class="nome">Painel</span>
-                </div>
-                <div class="elLista">
-                    <a href="javascript:void(0);">
-                        <img src="../icons/person-sharp - cópia.svg" alt="Perfil">
-                        <span class="nomeLink">Perfil</span>
-                    </a>
-                    <span class="nome">Perfil</span>
-                </div>
-                <div class="elLista">
-                    <a href="javascript:void(0);">
-                        <img src="../icons/medical-outline - cópia.svg" alt="Consulta">
-                        <span class="nomeLink">Consulta</span>
-                    </a>
-                    <span class="nome">Consulta</span>
-                </div>
-                <div class="elLista">
-                    <a href="javascript:void(0);">
-                        <img src="../icons/file-tray-full-outline - cópia.svg" alt="Histórico">
-                        <span class="nomeLink">Histórico</span>
-                    </a>
-                    <span class="nome">Histórico</span>
-                </div>
-                <div class="elLista perfil">
-                    <div class="detalhesPerfil">
-                        <img src="../icons/person-sharp - cópia.svg" alt="Imagem do Perfil" id="fotoPerfil">
-                        <div>
-                            <div class="nomePerfil">Mauro Neto</div>
-                            <div class="profissao">Dj</div>
-                        </div>
-                    </div>
-                    <img src="../icons/exit-outline - cópia.svg" alt="Sair" id="sair">
->>>>>>> eb6fa4a5faaeb17d86b579aaf38062a44e02423f
                 </div>
                 <img src="../icons/exit-outline - cópia.svg" alt="Sair" id="sair">
-                <span class="nome">Sair</span>
             </div>
+            <img src="../icons/exit-outline - cópia.svg" alt="Sair" id="sair">
+            <span class="nome">Sair</span>
         </div>
+    </div>
     </div>
     <div class="conteudoInterfaceUsuario">
 
@@ -156,18 +105,6 @@
 
             </div>
         </div>
-<<<<<<< HEAD
-
-=======
-        <div class="painel">
-            <img src="../icons/" alt="Marcar Consulta">
-            <span class="">Marcar Consulta</span>
-        </div>
-        <div class="painel">
-            <img src="../icons/document-attach-outline - cópia.svg" alt="Marcar Consulta">
-            <span class="">Histórico de Consultas</span>
-        </div>
->>>>>>> eb6fa4a5faaeb17d86b579aaf38062a44e02423f
         <script src="../js/script.js"></script>
 </body>
 

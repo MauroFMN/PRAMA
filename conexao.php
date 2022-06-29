bbd7c515f016dac9774a313762521286a0eafc08
MauroFMN <56952407+MauroFMN@users.noreply.github.com>
2022-06-29 11:28:01 +0100

    Alterações

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
   $db_host = 'localhost';
   $db_user = 'root';
-  $db_password = '';
+  $db_password = 'root';
   $db_db = 'praman';
-  $db_port = 3306;
+  $db_port = 8889;
 
   $mysqli = new mysqli(
     $db_host,
